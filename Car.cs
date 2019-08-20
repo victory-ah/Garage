767e7e6c11423efce4b75ee5df191d1d5e17cda1
Victoria Joseph <victoria.f.joseph@outlook.com>
2019-08-20 13:16:17 -0700

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Garage
+{
+    public class Car
+    {
+        //these are fields
+        public int nrWheels;
+        public string color;
+        public int nrDoors;
+        public bool isRunning;
+        string carName;
+
+        public string Name {
+            get=> carName;
+            set=> this.carName = value;
+        }
+
+        public Car()
+        {
+            this.color = "white";
+            this.isRunning = false;
+            this.nrDoors = 2;
+            this.nrWheels = 4;
+            this.carName = null;
+        }
+        //these are constructors
+        public Car(string newColor) : this()
+        {
+            this.color = newColor;
+        }
+        public Car(string newColor, string newName) : this()
+        {
+            this.color = newColor;
+            this.NameMyCar(newName);
+        }
+
+        public void FlatTire()
+        {
+            this.nrWheels--;
+        }
+        public void NameMyCar(string newName)
+        {
+            this.Name = newName;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Garage
+{
+    class Program
+    {
+        static void TheCars()
+        {
+            Car cardi = BuyACar("Black");
+            cardi.NameMyCar("Cardi");
+            cardi.nrDoors = 2;
+            cardi.nrWheels = 4;
+
+
+            Car abigail = BuyACar("Daytona Sunset");
+            abigail.NameMyCar("Abigail");
+            abigail.nrDoors = 2;
+            abigail.nrWheels = 4;
+
+            Car rentalCar = new Car();
+            rentalCar.NameMyCar("RENTAL CAR");
+            rentalCar.nrDoors = 4;
+            rentalCar.nrWheels = 4;
+
+            Car pearl = new Car("Pearl White");
+            pearl.NameMyCar("Pearl");
+            pearl.nrDoors = 4;
+            pearl.nrWheels = 4;
+
+            //cardi.FlatTire();
+            ParkMyCar(cardi);
+            ParkMyCar(abigail);
+            ParkMyCar(rentalCar);
+            ParkMyCar(pearl);
+        }
+      
+        static Car BuyACar(string carColor = "silver")
+        {
+            Car myNewCar = new Car(carColor);
+            return myNewCar;
+        }
+
+        static void ParkMyCar(Car myCar)
+        {
+            Console.WriteLine("Car: {0} is {1} it has {2} doors and {3} wheels",
+
+            myCar.Name,
+            myCar.color,
+            myCar.nrDoors,
+            myCar.nrWheels
+            );
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Garage.Program.Main()");
+            TheCars();
+
+            
+        }
+    }
+}
